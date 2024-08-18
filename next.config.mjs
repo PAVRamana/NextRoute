8ab3d8c0f18897bf4d4bc474640feee35b2be20b
Ramana <ramana.ponnaganti@gmail.com>
2024-08-18 17:49:02 +0800

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   env: {
-    ISC_BASE_API_URL: 'https://company9994-poc.api.identitynow-demo.com',
-    ISC_BASE_URL: 'https://company9994-poc.identitynow-demo.com',
+    ISC_BASE_API_URL: 'https://123-poc.api.identitynow-demo.com',
+    ISC_BASE_URL: 'https://123-poc.identitynow-demo.com',
     ISC_CLIENT_ID: 'b8cef674-b262-4cbe-99af-8ee14e9d2395',
     ISC_CLIENT_SECRET:
       '3e4ca4ff65dae3b9a7849de475b1737936b951edeb6ad505f5c61b76acc47224',
