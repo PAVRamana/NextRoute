72308d1993ea012d38bb5183659d1c7fa8bc4171
Ramana <ramana.ponnaganti@gmail.com>
2024-08-18 17:47:40 +0800

    dd

---
+++ env/dev/next.config.mjs
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  env: {
+    ISC_BASE_API_URL: 'https://company9994-poc.api.identitynow-demo.com',
+    ISC_BASE_URL: 'https://company9994-poc.identitynow-demo.com',
+    ISC_CLIENT_ID: 'b8cef674-b262-4cbe-99af-8ee14e9d2395',
+    ISC_CLIENT_SECRET:
+      '3e4ca4ff65dae3b9a7849de475b1737936b951edeb6ad505f5c61b76acc47224',
+    NEXTAUTH_SECRET: 'khfhhjfhwie57whewiky34thiuty3489thiug',
+    NEXTAUTH_URL: 'http://localhost:3000',
+  },
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+        path: false,
+        os: false,
+      },
+    };
+    return config;
+  },
+};
