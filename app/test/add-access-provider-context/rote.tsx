6a7f1d284a6d5fde38dcf9aec88dbfd301d96a80
PAVRamana <ramana.ponnaganti@gmail.com>
2024-10-14 23:56:52 +0800

    Update rote.tsx

--- app/test/add-access-provider-context/rote.tsx
+++ app/test/add-access-provider-context/rote.tsx
@@ -1,69 +1,27 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Spinner } from '@wayfarer/components';
-import { default_theme } from 'exp-ui-web-components-mfe';
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { AddAccessDataProvider } from './common/add-access-provider-context';
-import { useFetchAddAccessData } from './common/add-access-provider-context/fetchData.hook';
-import { GlobalStyles } from './common/global-styles';
-import {
-  initialSelectedEntitlementAccountsContext,
-  SelectedEntitlementAccountsContextProvider,
-} from './common/selected-accounts-context';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from 'axios';
 
-import {
-  initialSelectedEntitlementsContext,
-  SelectedEntitlementsContextProvider,
-} from './common/selected-entitlement-context';
-import {
-  initialSelectedFormDataContext,
-  SelectedFormDataContextProvider,
-} from './common/selected-formdata-context';
-import {
-  initialSelectedUsersContext,
-  SelectedUsersContextProvider,
-} from './common/selected-users-context';
-import PageLayout from './page-layout';
+const baseURL = window.location.origin;
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
+const api = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
-export default function App() {
-  const addAccessProviderData = useFetchAddAccessData();
-
-  return (
-    <React.Fragment>
-      {!addAccessProviderData ? (
-        <Spinner appearance='branded' active />
-      ) : (
-        <QueryClientProvider client={queryClient}>
-          <AddAccessDataProvider values={addAccessProviderData}>
-            <SelectedUsersContextProvider params={initialSelectedUsersContext}>
-              <SelectedEntitlementsContextProvider
-                params={initialSelectedEntitlementsContext}
-              >
-                <SelectedEntitlementAccountsContextProvider
-                  params={initialSelectedEntitlementAccountsContext}
-                >
-                  <SelectedFormDataContextProvider
-                    params={initialSelectedFormDataContext}
-                  >
-                    <ThemeProvider theme={default_theme}>
-                      <GlobalStyles />
-                      <PageLayout />
-                    </ThemeProvider>
-                  </SelectedFormDataContextProvider>
-                </SelectedEntitlementAccountsContextProvider>
-              </SelectedEntitlementsContextProvider>
-            </SelectedUsersContextProvider>
-          </AddAccessDataProvider>
-        </QueryClientProvider>
-      )}
-    </React.Fragment>
+function createAxiosResponseInterceptor(axiosInstance: any) {
+  const interceptor = axiosInstance.interceptors.response.use(
+    (response: any) => response,
+    (error: any) => {
+      if (error?.response?.status === 401) {
+        window.open('/identityiq/home.jsf', '_self');
+      }
+      axiosInstance.interceptors.response.eject(interceptor);
+    }
   );
 }
+
+createAxiosResponseInterceptor(api);
+
+export default api;
