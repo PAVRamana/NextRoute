b5d8f36d7a553b3ab7c553f9974f3db362b83b7b
Venkat <ramana.ponnaganti@gmail.com>
2025-01-15 22:22:05 +0800

    dsds

--- app/test/index.tsx
+++ app/test/index.tsx
@@ -1,25 +1,13 @@
-
-Token expire check in all pages - invoke user api and redirect to 401 and to same route
-Comments check
-Refetch all clear on all places
-
-Removed circle for User image and Empty Image
-Added Link for Email
-Fixed Attibutes alignment issue when browser resize
-Status changed to Capital case
-Assigned On added on Pending table
-Actioned On added on Completed table
-Changed label state to status on Completed table
-Request details icon changed
-Description, business Just - Diplsayed Not Availble instaead of None
-My Groups and My Okta Cards added on Home page
-Bulk Approval/Reject
-
-
-Sorting supported for created and modofied only both pending and completed API's
-
-Manger Request
- Request reaasign - Changed to loggedin user token 
- Assigned to user - Changed to loggedin user token 
-
-Certifications and My Approvals - We have to use loggedin user token right?
+curl --location 'https://point72-sb.api.identitynow.com/v3/search' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRfaWQiOiI5ZWNlYTRlZC1jZDhjLTQ4MzMtYmU2Ny04MTY3NDU0M2MyZDQiLCJwb2QiOiJzdGcwMy11c2Vhc3QxIiwic3Ryb25nX2F1dGhfc3VwcG9ydGVkIjpmYWxzZSwib3JnIjoicG9pbnQ3Mi1zYiIsImlkZW50aXR5X2lkIjoiNTMwYTVjYTM0YzM4NDJhMmEwYWFjYzNjOGJhYWVhMmMiLCJ1c2VyX25hbWUiOiJSYW5qYW4uRGFsYWkiLCJzY29wZSI6WyJCZz09Il0sInN0cm9uZ19hdXRoIjp0cnVlLCJleHAiOjE3MzY5OTM2NjEsImF1dGhvcml0aWVzIjpbIk9SR19BRE1JTiIsInNwOnVzZXIiXSwianRpIjoiUFBFaXhCcGh3bzRaVXpRaTc4eG9ZcXNFTUZvIiwiY2xpZW50X2lkIjoiZjg1ZjE1ODRhOWRmNGIyZTk3NjdlMzdhZGIxY2E1ODkifQ.huyNL-6E-ohpb_6VuanT1-ubLLGiwP3CDs0BUqxPFBc' \
+--data '{
+  "queryDsl": {
+    "query_string": {
+      "query": "attributes.interface:access-sunrise-sunset AND operation:REMOVE AND created:[now-1d/d TO now]"
+    }
+  },
+  "indices": ["events"],
+  "queryType": "DSL"
+}
+'
